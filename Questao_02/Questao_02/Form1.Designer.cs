293cf3aae46b7e270e555b400ad266c3801a5256
VitorArgolo <83723061+VitorArgolo@users.noreply.github.com>
2022-02-20 14:24:37 -0300

    Add files via upload

---
+++ Questao_02/Questao_02/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Questao_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.tb_senha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(103, 79);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(108, 30);
+            this.btn_cadastrar.TabIndex = 0;
+            this.btn_cadastrar.Text = "Cadastrar Senha";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            this.btn_cadastrar.Click += new System.EventHandler(this.btn_cadastrar_Click);
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(74, 21);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(137, 20);
+            this.tb_nome.TabIndex = 1;
+            this.tb_nome.Text = "Débora";
+            // 
+            // tb_senha
+            // 
+            this.tb_senha.Location = new System.Drawing.Point(74, 53);
+            this.tb_senha.Name = "tb_senha";
+            this.tb_senha.PasswordChar = '*';
+            this.tb_senha.Size = new System.Drawing.Size(137, 20);
+            this.tb_senha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 145);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_senha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Questão 02";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.TextBox tb_senha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Questao_02/Questao_02/Form1.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questao_02
+{
+    public partial class Form1 : Form
+    {
+        List<Usuario> listaUsuario = new List<Usuario>();
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists("dadosUsuario.json"))
+            {
+                CarregarArquivo();
+            }
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario usuario = new Usuario()
+                {
+                    Nome = tb_nome.Text,
+                    Senha = tb_senha.Text
+                };
+
+                listaUsuario.Add(usuario);
+                GravarArquivo();
+                MessageBox.Show("Salvo");
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+        /// <summary>
+        ///  Serializa e grava a lista em arquivo "dadosUsuario.json".
+        /// </summary>
+        private void GravarArquivo()
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+            string conteudoMarca = JsonConvert.SerializeObject(listaUsuario, settings);
+            File.WriteAllText("dadosUsuario.json", conteudoMarca);
+        }
+        /// <summary>
+        /// Desserializa e carrega o arquivo "dadosUsuario.json" em lista.
+        /// </summary>
+        private void CarregarArquivo()
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+            string conteudo = File.ReadAllText("dadosUsuario.json");
+            listaUsuario = JsonConvert.DeserializeObject<List<Usuario>>(conteudo, settings);
+        }
+
+        private void rb_visualizarSenha_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
